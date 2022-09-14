eacc3b7cc4a128ad79ffe4ce17d45e99e89fe663
Ñeka, la gatita de tus sueños <90157474+neko-girl@users.noreply.github.com>
2022-09-14 11:08:40 -0400

    Miau

--- javascript.js
+++ javascript.js
@@ -30,7 +30,37 @@ let challenges = [
             'Quitate 3 prendas de ropa',
             'Pasar la lengua por el cuerpo de otra persona',
             'Realizar sentadillas en ropa interior',
-            'Pon una de tus películas porno favoritas en el movil'
+            'Pon una de tus películas porno favoritas en el movil',
+            
+            'Da una nalgada al jugador de la derecha',
+            'Da una nalgada al jugador de la izquierda',
+            'Seduce a una persona de tu mismo genero frente a todos',
+            'Juega lo que reste del juego abraz@ al jugador de la izquierda',
+            'Muerde despacito el labio al jugador de la derecha',
+            'Dale un beso en la oreja al jugador frente a ti',
+            'Sacale una prenda al jugador de la izquierda sin usar las manos',
+            'Cierra los ojos, otro jugador te tiene que dar un beso, despues tienes que adivinar quien fue',
+            'Ponte en 4 durante 3 turnos',
+            'sientate en otro jugador durante varios turnos',
+            'Da un beso en la parte del cuerpo que quieras a un jugador',
+            'Ponte en pose del misionero con el jugador de tu derecha',
+            'Acariciale la pierna al jugador de tu izquierda',
+            'Da un beso al jugador mas lejos de ti',
+            'Apoya tu cabeza sobre la entrepierna del jugador de tu izquierda durante 2 turnos',
+            'Un jugador tiene que hacer un chupeto en el cuello',
+            'Sientate sobre un jugador de espalda a los demas lo que reste del juego',
+            'Ponte en pose de 69 con el jugador de tu derecha',
+            'Haz un recorrido de besos sensuales al jugador de tu derecha desde la panza hasta la pelvis',
+            'Teoriza sobre el arte del porno, como dirigirias el video perfecto?',
+            'Da una catedra de como hacer una buena mamada',
+            'Beso intenso',
+            'Foto-Orgia, Todos los jugadores menos tu tienen que hacer una pose sexual con el jugador de su derecha, tu sacas la foto (si son impares sale trio)',
+            'Sacate una prenda',
+            'El jugador de la derecha tiene que apoyar su pie en tu entrepierna lo que reste del juego',
+            'Demuestra como seducirias a alguien',
+            'Tienes 5 segundos para decir los nombres de todos, dale un beso a los que no llegues a nombrar',
+            'Todos los jugadores te tienen que dar un beso en algun lugar del cuerpo, no vale repetir',
+            'Los jugadores quee esten a tu izquierda y a tu derecha tienen que inclinarse y poner su boca en tu entrepierna (de forma sensual) y quedarse ahi 2 turnos'
         ]
 let originalLeght = Math.round(challenges.length*0.85)
 const update = (items) => challenges = items;
